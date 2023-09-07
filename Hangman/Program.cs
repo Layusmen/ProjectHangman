5e874123362ce34233852db1bce111e17206abbf
Olayode Yusuff <getlayus@gmail.com>
2023-09-07 20:53:51 +0100

    few added code

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -19,7 +19,54 @@ namespace ProjectHangman
 
             Console.WriteLine("I have picked a secret word from the list, can you try to guess the word?:");
 
-            //Console.WriteLine(words[secretWordIndex]);
+            Console.WriteLine(words[secretWordIndex]);
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("Can you guess it?");
+
+            bool isGameOver = false;
+            int numberOfGuesses = 0;
+            string guessedLetters = "";
+
+            while (!isGameOver)
+            {
+                Console.WriteLine("Guess a letter:");
+                string guessedLetter = Console.ReadLine();
+
+                if (guessedLetters.Contains(guessedLetter))
+                {
+                    Console.WriteLine("You already guessed that letter!");
+                }
+                else
+                {
+                    guessedLetters += guessedLetter;
+
+                    bool isLetterInWord = secretWord.Contains(guessedLetter);
+
+                    if (isLetterInWord)
+                    {
+                        Console.WriteLine("The letter " + guessedLetter + " is in the secret word!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The letter " + guessedLetter + " is not in the secret word!");
+                        numberOfGuesses++;
+                    }
+
+                    if (numberOfGuesses == 6)
+                    {
+                        isGameOver = true;
+                        Console.WriteLine("You lose!");
+                    }
+                }
+
+            }
+
+
+
         }
     }
 }
