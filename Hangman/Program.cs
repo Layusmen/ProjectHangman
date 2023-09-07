6190114dab75171b4ad00f57a013caefb1afb470
Olayode Yusuff <getlayus@gmail.com>
2023-09-07 20:25:25 +0100

    A few added code

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Hangman
+﻿using System;
+namespace ProjectHangman
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            List<string> words = new List<string>();
+            words.Add("Standard");
+            words.Add("Approach");
+            words.Add("Wonder");
+            words.Add("Student");
+            words.Add("catering");
+
+            Random random = new Random();
+            int secretWordIndex = random.Next(words.Count);
+            string secretWord = words[secretWordIndex];
+
+            Console.WriteLine("I have picked a secret word from the list, can you try to guess the word?:");
+
+            //Console.WriteLine(words[secretWordIndex]);
         }
     }
 }
